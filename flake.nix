c4a21c8a2d3fb33df8dbd0c1721da7c04f4d67c4
BeLeap <beleap@beleap.dev>
2025-11-05 01:06:25 +0900

    try 1

--- flake.nix
+++ flake.nix
@@ -3,19 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
-      self,
-      nixpkgs,
-      flake-utils,
       ...
     }:
-    flake-utils.lib.simpleFlake {
-      inherit self nixpkgs;
-      name = "beleap-nix-fonts";
-      overlay = ./overlay.nix;
+    {
+      overlays = {
+        default = ./overlay.nix;
+      };
     };
 }
