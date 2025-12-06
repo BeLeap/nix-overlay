06998d4b4a6fc4460a4291348f2759915d61fc24
Changseo Jang <beleap@beleap.dev>
2025-12-06 21:13:57 +0900

    Fix pchar build on Darwin

--- pkgs/pchar.nix
+++ pkgs/pchar.nix
@@ -25,13 +25,21 @@ stdenv.mkDerivation rec {
 
     substituteInPlace pc.h \
       --replace '#if (SIZEOF_BOOL == 0)' '#if (SIZEOF_BOOL == 0) && !defined(__cplusplus)'
+
+    substituteInPlace Pctest.cc \
+      --replace '#ifdef HAVE_BPF\n#include <sys/ioctl.h>' '#ifdef HAVE_BPF\n#include <sys/ioctl.h>\n#include <net/bpf.h>' \
+      --replace 'register int sum = 0;' 'int sum = 0;' \
+      --replace '        u_int immediate = 1;\n        if (ioctl(fileno, BIOCIMMEDIATE, &immediate) < 0) {\n            perror("ioctl(BIOCIMMEDIATE)");\n            exit(1);\n        }\n#endif /* HAVE_BPF */' '        u_int immediate = 1;\n#ifdef BIOCIMMEDIATE\n        if (ioctl(fileno, BIOCIMMEDIATE, &immediate) < 0) {\n            perror("ioctl(BIOCIMMEDIATE)");\n            exit(1);\n        }\n#endif\n#endif /* HAVE_BPF */'
   '';
 
   configureFlags = [
     "--with-ipv6"
     "--with-pcap=${lib.getDev libpcap}"
-    "ac_cv_sizeof_bool=1"
   ];
+
+  preConfigure = ''
+    export ac_cv_sizeof_bool=1
+  '';
 
   meta = with lib; {
     description = "Network path characterization tool";
