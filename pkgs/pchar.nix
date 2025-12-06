f9c24627e7a7f0f210742da5ff126be17ee3e2af
Changseo Jang <beleap@beleap.dev>
2025-12-06 18:35:35 +0900

    Fix pchar build on macOS

--- pkgs/pchar.nix
+++ pkgs/pchar.nix
@@ -12,9 +12,25 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ libpcap ];
 
+  postPatch = ''
+    mv VERSION PCHAR_VERSION
+
+    substituteInPlace configure.in configure \
+      --replace 'cat ${srcdir}/VERSION' 'cat ${srcdir}/PCHAR_VERSION'
+
+    substituteInPlace Makefile.in \
+      --replace 'VERSION \\\\' 'PCHAR_VERSION \\\\' \
+      --replace 'version.cc: VERSION' 'version.cc: PCHAR_VERSION' \
+      --replace '$(srcdir)/VERSION' '$(srcdir)/PCHAR_VERSION'
+
+    substituteInPlace pc.h \
+      --replace '#if (SIZEOF_BOOL == 0)' '#if (SIZEOF_BOOL == 0) && !defined(__cplusplus)'
+  '';
+
   configureFlags = [
     "--with-ipv6"
     "--with-pcap=${lib.getDev libpcap}"
+    "ac_cv_sizeof_bool=1"
   ];
 
   meta = with lib; {
