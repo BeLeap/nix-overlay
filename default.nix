f14278aa3593c7c9eb4b8c63949fa88608cfa8ae
BeLeap <beleap@beleap.dev>
2026-02-20 07:49:41 +0900

    update java

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ final: prev: {
   dnsi = prev.callPackage ./pkgs/dnsi.nix { };
   kdeconnect-mac = prev.callPackage ./pkgs/kdeconnect-mac.nix { };
   empiriqa = prev.callPackage ./pkgs/empiriqa.nix { };
-  kotlin-lsp = prev.callPackage ./pkgs/kotlin-lsp.nix { };
+  kotlin-lsp = prev.callPackage ./pkgs/kotlin-lsp { };
   envoy-tahoe = prev.callPackage ./pkgs/envoy-tahoe.nix { };
   kubectl-sniff = prev.callPackage ./pkgs/kubectl-sniff.nix { };
   wezterm-null = prev.callPackage ./pkgs/wezterm { };
