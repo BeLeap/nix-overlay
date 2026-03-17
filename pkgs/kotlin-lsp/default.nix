9c2b4fa04cdcf00d2b0fcbdc18cee47635e1ca05
Changseo Jang <beleap@beleap.dev>
2026-03-17 10:24:36 +0900

    update kotlin-lsp (#13)
    * cleanup
    
    * format
    
    * cleanup patchfile
    
    * update lib using patch
    
    * change patchfile name
    
    * update url
    
    * update version

--- pkgs/kotlin-lsp/default.nix
+++ pkgs/kotlin-lsp/default.nix
@@ -3,49 +3,48 @@
   fetchzip,
   pkgs,
   lib,
-}:
-let
+}: let
   java = pkgs.javaPackages.compiler.temurin-bin.jre-21;
 in
-stdenvNoCC.mkDerivation rec {
-  name = "kotlin-lsp";
-  version = "261.13587.0";
-  src = fetchzip {
-    # origin "https://download-cdn.jetbrains.com/kotlin-lsp/{{ version }}/kotlin-lsp-{{ version }}-mac-aarch64.zip";
-    url = "https://github.com/BeLeap/nix-overlay/releases/download/${name}-${version}/kotlin-lsp-${version}-mac-aarch64.zip";
-    stripRoot = false;
-    hash = "sha256-zwlzVt3KYN0OXKr6sI9XSijXSbTImomSTGRGa+3zCK8=";
-  };
+  stdenvNoCC.mkDerivation rec {
+    name = "kotlin-lsp";
+    version = "262.1817.0";
+    src = fetchzip {
+      # origin
+      # url = "https://download-cdn.jetbrains.com/kotlin-lsp/${version}/kotlin-lsp-${version}-mac-aarch64.zip";
+      url = "https://github.com/BeLeap/nix-overlay/releases/download/${name}-${version}/kotlin-lsp-${version}-mac-aarch64.zip";
+      stripRoot = false;
+      hash = "sha256-zwlzVt3KYN0OXKr6sI9XSijXSbTImomSTGRGa+3zCK8=";
+    };
 
-  patches = [
-    ./patches/kotlin-lsp-respect-local-jre-path.patch
-  ];
+    patches = [
+      ./patches/lib-path.patch
+      ./patches/local-jre-path.patch
+    ];
 
-  nativeBuildInputs = with pkgs; [
-    makeWrapper
-    patchelf
-    java
-  ];
+    nativeBuildInputs = with pkgs; [
+      makeWrapper
+      patchelf
+      java
+    ];
 
-  installPhase = ''
-    mkdir -p $out/bin
+    installPhase = ''
+      mkdir -p $out/bin
 
-    sed -i 's/\/lib/\/..\/lib/' kotlin-lsp.sh
+      install -m +x kotlin-lsp.sh $out/bin/kotlin-lsp
+      mv lib $out/lib
+      mv native $out/native
 
-    install -m +x kotlin-lsp.sh $out/bin/kotlin-lsp
-    mv lib $out/lib
-    mv native $out/native
+      wrapProgram $out/bin/kotlin-lsp \
+        --prefix PATH : ${lib.makeBinPath [java]} \
+        --set-default LOCAL_JRE_PATH ${java}
+    '';
 
-    wrapProgram $out/bin/kotlin-lsp \
-      --prefix PATH : ${lib.makeBinPath [ java ]} \
-      --set-default LOCAL_JRE_PATH ${java}
-  '';
+    checkPhase = ''
+      kotlin-lsp --help
+    '';
 
-  checkPhase = ''
-    kotlin-lsp --help
-  '';
-
-  meta = {
-    mainProgram = "kotlin-lsp";
-  };
-}
+    meta = {
+      mainProgram = "kotlin-lsp";
+    };
+  }
