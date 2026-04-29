cbf1212db71ccf5ee736d40718cc2d19c90c4e25
Changseo Jang <beleap@beleap.dev>
2026-04-29 11:04:50 +0900

    update kubectl-sniff (#23)
    * add go packaging
    
    * add static-tcpdump
    
    * add makeWrapper to nativeBuildInputs

--- pkgs/kubectl-sniff.nix
+++ pkgs/kubectl-sniff.nix
@@ -1,46 +1,71 @@
 {
-  stdenvNoCC,
-  fetchurl,
-  stdenv,
-  pkgs,
-}:
-let
-  mapping = {
-    "aarch64-darwin" = "kubectl-sniff-darwin-arm64";
-    "x86_64-darwin" = "kubectl-sniff-darwin";
-    "x86_64-linux" = "kubectl-sniff";
-  };
-  binary = mapping.${stdenv.hostPlatform.system};
+  lib,
+  makeWrapper,
+  buildGoModule,
+  fetchFromGitHub,
+  pkgsCross,
+}: let
+  static-tcpdump = pkgsCross.musl64.pkgsStatic.callPackage (
+    {
+      lib,
+      stdenv,
+      fetchurl,
+      libpcap,
+      pkg-config,
+      perl,
+    }:
+      stdenv.mkDerivation (finalAttrs: {
+        pname = "tcpdump";
+        version = "4.99.6";
+
+        src = fetchurl {
+          url = "https://www.tcpdump.org/release/tcpdump-${finalAttrs.version}.tar.gz";
+          hash = "sha256-WDmSGg9n19j6PazZzUHkTInMuGfoptshbWJijH/RSwk=";
+        };
+
+        postPatch = ''
+          patchShebangs tests
+        '';
+
+        nativeBuildInputs = [pkg-config];
+
+        nativeCheckInputs = [perl];
+
+        buildInputs = [libpcap];
+
+        configureFlags = lib.optional (stdenv.hostPlatform != stdenv.buildPlatform) "ac_cv_linux_vers=2";
+
+        meta = {
+          description = "Network sniffer";
+          homepage = "https://www.tcpdump.org/";
+          license = lib.licenses.bsd3;
+          maintainers = with lib.maintainers; [globin];
+          platforms = lib.platforms.unix;
+          mainProgram = "tcpdump";
+        };
+      })
+  ) {};
 in
-stdenvNoCC.mkDerivation rec {
-  pname = "kubectl-sniff";
-  version = "1.6.2";
+  buildGoModule rec {
+    pname = "kubectl-sniff";
+    version = "1.6.2";
 
-  nativeBuildInputs = with pkgs; [
-    unzip
-  ];
+    nativeBuildInputs = [makeWrapper];
 
-  src = fetchurl {
-    url = "https://github.com/eldadru/ksniff/releases/download/v${version}/ksniff.zip";
-    hash = "sha256-xZtcnqhNbLdxCW8SRskZtxOJ+dQjToWPSSkgiVflYf0=";
-  };
+    src = fetchFromGitHub {
+      owner = "eldadru";
+      repo = "ksniff";
+      rev = "v${version}";
+      hash = "sha256-Dz+qnpUKdhNdYC74lqUZXwCk73jb6pY2tIGjtTvNiUQ=";
+    };
 
-  unpackPhase = ''
-    runHook preUnpack
-    unzip $src
-    ls -alR || true
-    runHook postUnpack
-  '';
+    vendorHash = "sha256-7pSpOF8UASWqRMWaomoUBA3pD8t0qWiaIcGlXEm0Yx0=";
 
-  installPhase = ''
-    runHook preInstall
+    doCheck = false;
 
-    mkdir -p $out/bin
-    install -m755 ${binary} $out/bin/kubectl-sniff
-    install -m755 static-tcpdump $out/bin/static-tcpdump
-
-    runHook postInstall
-  '';
-
-  dontBuild = true;
-}
+    postInstall = ''
+      mv $out/bin/cmd $out/bin/kubectl-sniff
+      wrapProgram $out/bin/kubectl-sniff \
+        --set KUBECTL_PLUGINS_LOCAL_FLAG_LOCAL_TCPDUMP_PATH ${lib.getExe static-tcpdump}
+    '';
+  }
