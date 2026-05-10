0a7717f1692fb50aec2ea1eb0938fc66e531dd7d
Changseo Jang <beleap@beleap.dev>
2026-05-10 18:56:49 +0900

    upgrade rusty-kotlin-lsp (#24)

--- pkgs/rusty-kotlin-lsp.nix
+++ pkgs/rusty-kotlin-lsp.nix
@@ -7,14 +7,14 @@
 rustPlatform.buildRustPackage rec {
   pname = "rusty-kotlin-lsp";
   name = pname;
-  version = "0.2.1";
+  version = "0.12.1";
   src = fetchFromGitHub {
     owner = "Hessesian";
     repo = "kotlin-lsp";
-    rev = "b0281d3306491f4644d6014a381dcb7907d1afee";
-    hash = "sha256-zEe/q/ADwwfRqed/Gf7LUlyHQvWNzMMIlDwgyv5QJ64=";
+    rev = "v${version}";
+    hash = "sha256-r2xny76NtvIi9zCs0JKdpiCglFbJlzf7eFL4iBGrdAc=";
   };
-  cargoHash = "sha256-WBABvB/hbbNILLh7sRH2nSt9ubkaeVBDw7ZJFRcM2MY=";
+  cargoHash = "sha256-lViQHU33YeNl8K6UV83fhHCiCO11ppSzUgzKc0ruwu4=";
 
   nativeBuildInputs = with pkgs; [
     makeWrapper
