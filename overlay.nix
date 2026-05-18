c19e54fadbe2392c0e7da694abe506c26e75847d
BeLeap <beleap@beleap.dev>
2026-05-18 10:13:38 +0900

    use pinned pkgs for all pkgs

--- overlay.nix
+++ overlay.nix
@@ -9,17 +9,17 @@
 in {
   kubectl-check = kubectl-check-flake.packages.${final.stdenv.hostPlatform.system}.default;
   boda = boda-flake.packages.${final.stdenv.hostPlatform.system}.default;
-  nanum-myeongjo = prev.callPackage ./pkgs/nanum-myeongjo.nix {};
-  dnsi = prev.callPackage ./pkgs/dnsi.nix {};
-  empiriqa = prev.callPackage ./pkgs/empiriqa.nix {};
-  kotlin-lsp = prev.callPackage ./pkgs/kotlin-lsp {};
-  kubectl-sniff = prev.callPackage ./pkgs/kubectl-sniff.nix {inherit pinnedPkgs;};
-  pchar = prev.callPackage ./pkgs/pchar.nix {};
-  wezterm-null = prev.callPackage ./pkgs/wezterm {};
-  joplin-terminal = prev.callPackage ./pkgs/joplin-terminal {inherit (pinnedPkgs) joplin-cli;};
-  rusty-kotlin-lsp = prev.callPackage ./pkgs/rusty-kotlin-lsp.nix {};
+  nanum-myeongjo = pinnedPkgs.callPackage ./pkgs/nanum-myeongjo.nix {};
+  dnsi = pinnedPkgs.callPackage ./pkgs/dnsi.nix {};
+  empiriqa = pinnedPkgs.callPackage ./pkgs/empiriqa.nix {};
+  kotlin-lsp = pinnedPkgs.callPackage ./pkgs/kotlin-lsp {};
+  kubectl-sniff = pinnedPkgs.callPackage ./pkgs/kubectl-sniff.nix {};
+  pchar = pinnedPkgs.callPackage ./pkgs/pchar.nix {};
+  wezterm-null = pinnedPkgs.callPackage ./pkgs/wezterm {};
+  joplin-terminal = pinnedPkgs.callPackage ./pkgs/joplin-terminal {};
+  rusty-kotlin-lsp = pinnedPkgs.callPackage ./pkgs/rusty-kotlin-lsp.nix {};
 
-  kdeconnect-mac = prev.callPackage ./pkgs/kdeconnect-mac.nix {};
-  envoy-tahoe = prev.callPackage ./pkgs/envoy-tahoe.nix {};
-  google-messages = prev.callPackage ./pkgs/google-messages.nix {};
+  kdeconnect-mac = pinnedPkgs.callPackage ./pkgs/kdeconnect-mac.nix {};
+  envoy-tahoe = pinnedPkgs.callPackage ./pkgs/envoy-tahoe.nix {};
+  google-messages = pinnedPkgs.callPackage ./pkgs/google-messages.nix {};
 }
