adc4dd63df4bf372cd4323026be21291977c7518
Changseo Jang <beleap@beleap.dev>
2026-05-25 23:15:05 +0900

    Refactor Nanum Myeongjo font installation
    Updated the installation process for the Nanum Myeongjo font, including pre-install and post-install hooks.

--- pkgs/nanum-myeongjo.nix
+++ pkgs/nanum-myeongjo.nix
@@ -4,14 +4,18 @@
 }:
 stdenvNoCC.mkDerivation {
   name = "nanum-myeongjo";
-  dontConfigue = true;
   dontUnpack = true;
   src = fetchurl {
     url = "https://hangeul.pstatic.net/hangeul_static/webfont/NanumMyeongjo/NanumMyeongjo.ttf";
     hash = "sha256-60qd4wIu0nRMBipMRTMuh3FUYr9y817cNP6EUptKwT0=";
   };
   installPhase = ''
-    mkdir -p $out/share/fonts/truetype
-    cp $src $out/share/fonts/truetype/
+    runHook preInstall
+    install -Dm644 $src $out/share/fonts/truetype/NanumMyeongjo.ttf
+    runHook postInstall
   '';
+  meta = {
+    description = "Nanum Myeongjo font";
+    platforms = lib.platforms.all;
+  };
 }
